0708e113057cd76a05706fd1c3f7e5817117225c
Maham-Suhail <maham.suhail@khired.com>
2020-05-29 07:23:03 +0500

    first commit

---
+++ src/main/java/com/example/demo/StudentController/StudentController.java
@@ -0,0 +1,52 @@
+package com.example.demo.StudentController;
+
+import com.example.demo.StudentService.StudentService;
+import com.example.demo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping("/students")
+    public List<Student> list(){
+       return service.findAll();
+    }
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> get(@PathVariable Integer id){
+        try{
+            Student student = service.get(id);
+            return new ResponseEntity<Student>(student,HttpStatus.OK);
+        }catch (NoSuchElementException exception)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/students")
+    public void add(@RequestBody Student student){
+        service.save(student);
+    }
+    @DeleteMapping("/students/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+    @PutMapping("/students/{id}")
+    public ResponseEntity<?> update(@RequestBody Student student,@PathVariable Integer id){
+        try{
+            service.save(student);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (NoSuchElementException exception)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+}
---
+++ src/main/java/com/example/demo/StudentService/StudentService.java
@@ -0,0 +1,29 @@
+package com.example.demo.StudentService;
+
+import com.example.demo.StudentRepository.StudentRepository;
+import com.example.demo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    public Student get(Integer id) {
+       return repository.findById(id).get();
+    }
+
+    public void save(Student student){
+        repository.save(student);
+    }
+    public void delete(Integer id){
+        repository.deleteById(id);
+    }
+}
